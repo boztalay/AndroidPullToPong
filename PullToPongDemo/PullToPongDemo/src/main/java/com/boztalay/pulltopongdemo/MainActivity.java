097457945b625f28bdf1af67900c6f5098aaec08
Ben Oztalay <boztalay@gmail.com>
2014-02-10 01:58:53 -0500

    In the middle of getting the demo to work

--- PullToPongDemo/PullToPongDemo/src/main/java/com/boztalay/pulltopongdemo/MainActivity.java
+++ PullToPongDemo/PullToPongDemo/src/main/java/com/boztalay/pulltopongdemo/MainActivity.java
@@ -1,15 +1,9 @@
 package com.boztalay.pulltopongdemo;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.*;
 
 public class MainActivity extends Activity {
 
