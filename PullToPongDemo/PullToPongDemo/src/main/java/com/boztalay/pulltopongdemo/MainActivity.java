52ec9709193135518be0da59103f6146a92f62aa
Ben Oztalay <boztalay@gmail.com>
2014-02-10 03:36:15 -0500

    Got the demo working, need to library-ify it now

--- PullToPongDemo/PullToPongDemo/src/main/java/com/boztalay/pulltopongdemo/MainActivity.java
+++ PullToPongDemo/PullToPongDemo/src/main/java/com/boztalay/pulltopongdemo/MainActivity.java
@@ -5,55 +5,39 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.*;
 
-public class MainActivity extends Activity {
+import com.boztalay.pulltopong.PongHeaderTransformer;
+
+import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
+import uk.co.senab.actionbarpulltorefresh.library.Options;
+import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout;
+import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
+
+public class MainActivity extends Activity implements OnRefreshListener {
+
+    private PullToRefreshLayout mPullToRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        mPullToRefreshLayout = (PullToRefreshLayout) findViewById(R.id.ptr_layout);
+        ActionBarPullToRefresh.from(this)
+                .options(Options.create()
+                        .scrollDistance(0.5f)
+                        .headerLayout(R.layout.pong_header)
+                        .headerTransformer(new PongHeaderTransformer())
+                        .build())
+                .allChildrenArePullable()
+                .listener(this)
+                .setup(mPullToRefreshLayout);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onRefreshStarted(View view) {
+        //Load some data!
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void stopRefreshingButtonPressed(View view) {
+        mPullToRefreshLayout.setRefreshComplete();
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
 }
